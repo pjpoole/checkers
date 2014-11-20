689d5ddb343b7b09c11966da89e9761d4b8deba3
Peter Poole <peter.j.poole@gmail.com>
2014-11-20 15:33:22 -0500

    diffs now point left and right. Prepare for moves to accept l, r commands.

--- pieces.rb
+++ pieces.rb
@@ -1,11 +1,10 @@
+require_relative 'checkers_errors.rb'
+
 class Piece
+
   # "white" moves from 0 toward (size - 1) (sense +1)
   # "black" moves from (size - 1) toward 0 (sense -1)
   COLORS = { w: 1, b: -1 }
-
-  # This is a constant because we'll be manipulating it a couple places.
-  # Only works for American and International Checkers, probably.
-  DIFFS = [[1,1],[-1,1]]
 
   attr_reader :pos, :color
 
@@ -13,20 +12,37 @@ class Piece
     @board, @pos, @color = board, pos, color
     @promoted = false
 
-    @move_diffs = DIFFS.map { |x, y| [x, y * COLORS[@color]] }
-    @jump_diffs = DIFFS.map { |x, y| [x * 2, y * 2 * COLORS[@color]] }
+    # Only works for American and International Checkers, probably.
+    diffs = [[-1,1],[1,1]]
+    # So. The way this is going, we want the left move to be on the left,
+    # and the right move to be on the right. So we'll reverse the moves if
+    # our color is black, so that from the player's point of view left is
+    # always on the left.
+    diffs.reverse! if color == :b
+
+    @slide_diffs = diffs.map { |x, y| [x, y * COLORS[@color]] }
+    @jump_diffs  = diffs.map { |x, y| [x * 2, y * 2 * COLORS[@color]] }
 
     @board[pos] = self
   end
 
+  # Inspection
   def to_s
-    return (@color == :w ? "O" : "X")
+    @color == :w ? "O" : "X"
   end
 
   def inspect
     @pos
   end
 
+
+  # Moving
+  def perform_moves!(move_sequence)
+
+  end
+
+  # TODO: Just implement four-way movement for pieces
+  # F*ck moving by co-ords
   def perform_slide(diff)
     end_pos = position_sum(@pos, diff)
     return false unless move_diffs.include?(end_pos)
@@ -40,8 +56,10 @@ class Piece
     end_pos = position_sum(@pos, diff)
     return false unless move_diffs.include?(end_pos)
 
+    jumped_pos = position_sum(@pos, diff.map { |coord| coord / 2 })
+
     @board[@pos], @board[end_pos] = nil, self
-    @board[position_sum(@pos, diff.map { |coord| coord / 2 } )] = nil
+    @board[jumped_pos] = nil
 
     @pos = end_pos
   end
@@ -54,8 +72,9 @@ class Piece
   def move_diffs
     moves = []
 
-    @move_diffs.each_with_index do |diff, i|
+    @slide_diffs.each_with_index do |diff, i|
       trial_move = position_sum(@pos, diff)
+      next if trial_move.any? { |x| !x.between?(0, @board.size - 1 ) }
 
       if @board[trial_move].nil?
         moves << trial_move
@@ -80,7 +99,7 @@ class Piece
 
     # take the diffs, negate them, and add them to the existing diffs.
     # Neat!
-    @move_diffs += @move_diffs.map { |x, y| [x, y * -1]}
-    @jump_diffs += @jump_diffs.map { |x, y| [x, y * -1]}
+    @slide_diffs += @slide_diffs.map { |x, y| [x, y * -1]}
+    @jump_diffs  += @jump_diffs.map  { |x, y| [x, y * -1]}
   end
 end
